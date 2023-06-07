c301a47cc1e10d3c91560889af83e11ae2fcfcd6
pth1209 <113861167+pth1209@users.noreply.github.com>
2023-06-07 07:26:45 -0500

    edit basic css

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -4,11 +4,11 @@ export const Navbar = () => {
     return (
         <div className = "Navbar">
             <Link to = "/">Home</Link>
-            <Link to = "/auth">Login/Register</Link>
             <Link to = "/create-recipe">Create Recipe</Link>
             <Link to = "/saved-recipe">Saved Recipe</Link>
             <Link to = "/enter-calorie">Log Calories</Link>
             <Link to = "/calorie-list">Calories</Link>
+            <Link to = "/auth">Login/Register</Link>
         </div>
     )
 }
