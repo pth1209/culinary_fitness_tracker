68d7b3ff61bc0c38a885e0c23e40f67dcf7a5494
pth1209 <113861167+pth1209@users.noreply.github.com>
2023-06-13 21:23:24 -0500

    edit calorie route

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, uniue: true},
     password: {type: String, required: true},
-    savedRecipes: [{type: mongoose.Schema.Types.ObjectId, ref: "recipes"}]
+    savedRecipes: [{type: mongoose.Schema.Types.ObjectId, ref: "recipes"}],
+    calories: [{type: mongoose.Schema.Types.ObjectId, ref: "calories"}]
 });
 
 export const UserModel = mongoose.model("users", UserSchema);
---
+++ backend/routes/calorie.js
@@ -0,0 +1,24 @@
+import express from "express"
+import {CalorieModel} from "../models/Calories.js"
+import { UserModel } from "../models/Users.js"
+
+const router = express.Router()
+
+router.post("/add", async (req, res) => {
+    const calories = new CalorieModel(req.body)
+    try {
+        const response = await calories.save({})
+        res.json(response)
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+router.get("/:ID", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.params.userID)
+        res.json({calories: user?.calories})
+    } catch (error) {
+        res.json(error)
+    }
+})
