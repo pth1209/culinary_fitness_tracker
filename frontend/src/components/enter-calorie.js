c762927a44b3f55f05ccb424dab6b65a3c3bc82b
pth1209 <113861167+pth1209@users.noreply.github.com>
2023-06-14 00:33:29 -0500

    edit calorie logging page

--- frontend/src/components/enter-calorie.js
+++ frontend/src/components/enter-calorie.js
@@ -1,3 +1,34 @@
+import axios from "axios"
+import { useState } from "react"
+import { getUserId } from "../hooks/getUserId"
+
+
 export const EnterCalorie= () => {
-    return <div>EnterCalorie</div>
+
+    const userID = getUserId()
+
+    const getTodayDate = () => {
+        const date = new Date()
+        return date.toISOString().split("T")[0]
+    }
+
+    const [calories, setCalories] = useState({
+        loggedCalories: 0,
+        loggedUser: userID,
+        loggedDate: getTodayDate()
+    })
+
+    const handleInputChange = (event) => {
+        setCalories(event.target.value)
+    }
+    
+
+    return <div className = "logCalories">
+        <h2>Enter Calories</h2>
+        <form>
+            <label></label>
+            <input type= "text" onChange = {handleInputChange}/>
+            <button type = "submit" className = "logButton">Log Calories</button>
+        </form>
+    </div>
 }
